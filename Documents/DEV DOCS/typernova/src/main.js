a69e22100133f9dec842df71f2ad19ea9e73b1fb
Abdussobur Abdurraheem <aaolayiwola2@gmail.com>
2026-09-17 18:37:04 +0100

    tested existing code

--- Documents/DEV DOCS/typernova/src/main.js
+++ Documents/DEV DOCS/typernova/src/main.js
@@ -1,5 +1,10 @@
 import { calculateEMA, calculateWPM } from "./typingEngine";
+// DOm selection
+let targetWord = "My name is Abdussobur";
+const textField = document.getElementById("text-display");
+textField.textContent = targetWord;
 
+// variables
 let typedHistory = [];
 const EMAConstant = 0.1;
 let smoothedSpeed = 0;
@@ -7,45 +12,43 @@ let rawWPM = 0;
 let correctCharacters = 0;
 let startTime = null; //stopwatch hasnt started yet
 let currentIndex = 0;
-let targetWord = "I know you love to play but you just have to make this right";
 
 window.addEventListener("keydown", (e) => {
   // check if this is the very firststroke to start the stopwatch
-    if (startTime === null) {
+  if (startTime === null) {
     startTime = Date.now();
-    }
+  }
 
   // validate if the character is correct and update the correctChars count
-    if (e.key === targetWord[currentIndex]) {
+  if (e.key === targetWord[currentIndex]) {
     correctCharacters++;
-    typedHistory.push(event.key);
+    typedHistory.push(e.key);
+  }
+  currentIndex++;
+
+  if (e.key === "Backspace") {
+    // move the count backward
+    currentIndex--;
+
+    // capture erasedChar
+    const erasedChar = typedHistory.pop();
+
+    // validate the correctness and deduct or leave correctWords count appropriately
+    if (erasedChar === targetWord[currentIndex]) {
+      correctCharacters--;
     }
-    currentIndex++;
-    
-    if (e.key === "Backspace") {
-        // move the count backward
-        currentIndex--;
-        
-        // capture erasedChar
-        const erasedChar = typedHistory.pop();
+  }
 
-        // validate the correctness and deduct or leave correctWords count appropriately
-        if (erasedChar === targetWord[currentIndex]) {
-            correctCharacters--;
-        }
-
-
-    }
   // calculate new speeds by calling new functions    r
-    let currentTime = Date.now();
-    rawWPM = calculateWPM(correctCharacters, startTime, currentTime);
-    smoothedSpeed = calculateEMA(EMAConstant, rawWPM, smoothedSpeed);
+  let currentTime = Date.now();
+  rawWPM = calculateWPM(correctCharacters, startTime, currentTime);
+  smoothedSpeed = calculateEMA(EMAConstant, rawWPM, smoothedSpeed);
 
-    console.log({
-    key: event.key,
+  console.log({
+    key: e.key,
     index: currentIndex,
     score: correctCharacters,
     wpm: rawWPM,
-    ema: smoothedSpeed
-});
+    ema: smoothedSpeed,
+  });
 });
--- Documents/DEV DOCS/typernova/src/typingEngine.js
+++ Documents/DEV DOCS/typernova/src/typingEngine.js
@@ -1,9 +1,10 @@
 export function calculateEMA(EMAConstant, rawWPM, smoothedSpeed) {
-  return  rawWPM * EMAConstant + smoothedSpeed * (1 - EMAConstant);
+  console.log(rawWPM * EMAConstant) + (smoothedSpeed * (1 - EMAConstant));
+  return (rawWPM * EMAConstant) + (smoothedSpeed * (1 - EMAConstant));
 }
 
 export function calculateWPM(correctCharacters, startTime, currentTime) {
-    const elapsedTime = ((currentTime - startTime) / 60000);
-    const correctWords = correctCharacters / 5;
+  const elapsedTime = ((currentTime - startTime) / 60000);
+  const correctWords = correctCharacters / 5;
     return (correctWords / elapsedTime);
 }
